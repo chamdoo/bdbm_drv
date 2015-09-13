8f3cbca6236b1f86c48a322b54d3eba440f684b3
Sungjin Lee <chamdoo@gmail.com>
2015-09-13 19:30:13 -0400

    added benchmarks

---
+++ run/mount_ext4_dftl.sh
@@ -0,0 +1,10 @@
+sudo mkdir -p /usr/share/bdbm_drv
+sudo touch /usr/share/bdbm_drv/ftl.dat
+sudo touch /usr/share/bdbm_drv/dm.dat
+
+sudo insmod risa_dev_ramdrive_intr.ko
+sudo insmod bdbm_drv_dftl.ko
+sudo ./bdbm_format /dev/blueDBM
+sudo mkfs -t ext4 -b 4096 /dev/blueDBM
+sudo mount \-t ext4 \-o discard /dev/blueDBM /media/blueDBM
+#sudo mount \-t ext4 /dev/blueDBM /media/blueDBM
