025eea607af1a1560ee4a403bea5fc9c25b61338
gdice05 <jspark@davinci.snu.ac.kr>
2016-09-30 17:07:01 +0900

    settings for Makefile and ramdrive_timing

--- frontend/kernel/mount_ext4.sh
+++ frontend/kernel/mount_ext4.sh
@@ -2,9 +2,9 @@ sudo mkdir -p /usr/share/bdbm_drv
 sudo touch /usr/share/bdbm_drv/ftl.dat
 sudo touch /usr/share/bdbm_drv/dm.dat
 
-#sudo insmod risa_dev_ramdrive_timing.ko
+sudo insmod ../../devices/ramdrive_timing/risa_dev_ramdrive_timing.ko
 #sudo insmod risa_dev_ramdrive_intr.ko
-sudo insmod risa_dev_bluedbm.ko
+#sudo insmod risa_dev_bluedbm.ko
 #sudo insmod bdbm_drv_page.ko
 sudo insmod bdbm_drv.ko
 sudo ./bdbm_format /dev/blueDBM
