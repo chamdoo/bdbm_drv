c2cb5fe3b8a61ee02cba918b453f75e2417d17ea
Youngdon Jung <yellowjung@naver.com>
2017-02-27 18:29:40 +0900

    sh: turn off trim

--- frontend/kernel/mount_ext4_ram.sh
+++ frontend/kernel/mount_ext4_ram.sh
@@ -5,4 +5,5 @@ sudo touch /usr/share/bdbm_drv/dm.dat
 sudo insmod risa_dev_ramdrive.ko
 sudo insmod robusta_drv.ko
 sudo mkfs -t ext2 -b 4096 /dev/robusta
-sudo mount -t ext2 -o discard /dev/robusta /media/robusta
+sudo mount -t ext2 /dev/robusta /media/robusta
+#sudo mount -t ext2 -o discard /dev/robusta /media/robusta
